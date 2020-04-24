0fd7d8067f855b0746d5dca2c1227c82ebf3c820
chrisspoll <62038377+chrisspoll@users.noreply.github.com>
2020-04-24 16:21:08 +0200

    Add files via upload

---
+++ Question-9/Telephone.java
@@ -0,0 +1,16 @@
+package assignments.assignment_14.Question_9;
+
+public class Telephone {
+
+	private String number;
+	public static int quantity = 250;
+	public static double total = 15658.92;
+	
+	public String getNumber() {
+		return number;
+	}
+	
+	public void setNumber(String number) {
+		this.number = number;
+	}
+}
