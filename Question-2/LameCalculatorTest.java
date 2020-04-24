bdef9d87d934582cdb446e312f266eed62995967
chrisspoll <62038377+chrisspoll@users.noreply.github.com>
2020-04-24 16:16:45 +0200

    Add files via upload

---
+++ Question-2/LameCalculatorTest.java
@@ -0,0 +1,16 @@
+package assignments.assignment_14.Question_2;
+
+public class LameCalculatorTest {
+
+	public static void main(String[] args) {
+		
+		LameCalculator lc = new LameCalculator();
+
+		System.out.println(lc.plus(1,1));
+		System.out.println(lc.minus(1,1));
+		System.out.println(lc.multiply(2,1));
+		System.out.println(lc.divide(10,2));
+
+	}
+
+}
