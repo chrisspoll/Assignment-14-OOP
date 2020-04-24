aa7ac2508b61d9cc281177bedd91a72bc1334087
chrisspoll <62038377+chrisspoll@users.noreply.github.com>
2020-04-24 16:21:38 +0200

    Add files via upload

---
+++ Question-10/Calculator.java
@@ -0,0 +1,14 @@
+package assignments.assignment_14.Question_10;
+
+public class Calculator {
+
+	public static int plus(int a,int b) {
+		return a+b;
+	}
+	
+	public static int minus(int a,int b) {
+		return a-b;
+	}
+	
+	
+}
